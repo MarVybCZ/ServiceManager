b515af1522afb38f38e28e4658e7b4da952d7de3
vybiral <vybiralm81@gmail.com>
2017-05-31 19:37:24 +0200

    Minor chanes

---
+++ ServiceManager/Controls/GridViewHeader.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServiceManager.Controls
+{
+    /// <summary>
+    /// Interaction logic for GridViewHeader.xaml
+    /// </summary>
+    public partial class GridViewHeader : UserControl
+    {
+        public static readonly DependencyProperty ColumnDataProperty = DependencyProperty.Register("ColumnData", typeof(string), typeof(GridViewTextColumn), new PropertyMetadata(""));
+
+        public string ColumnData
+        {
+            get { return (string)GetValue(ColumnDataProperty); }
+            set { SetValue(ColumnDataProperty, value); }
+        }
+
+        public GridViewHeader()
+        {
+            InitializeComponent();
+
+            LayoutRoot.DataContext = this;
+        }
+    }
+}
