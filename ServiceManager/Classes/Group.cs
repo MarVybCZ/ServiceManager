5e4a790b2d89057cf157d12bfbef7329045e50e4
vybiral <vybiralm81@gmail.com>
2018-01-11 16:17:01 +0100

    Group and services wrappers

---
+++ ServiceManager/Classes/Group.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceManager.Classes
+{
+    class Group
+    {
+        public string Name { get; set; }
+
+        public List<ServiceWrapper> Services { get; set; }
+
+        public int ID { get; set; }
+    }
+}
---
+++ ServiceManager/Classes/ServiceWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceManager.Classes
+{
+    class ServiceWrapper
+    {
+        public string ServiceName { get; set; }
+        
+        public List<int> Groups { get; set; }
+    }
+}
